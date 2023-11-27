d89722204d72fe4259bac39200885e582c6d68f8
greens12 <razasara21292@gmail.com>
2023-11-28 00:11:13 +0530

    emp added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,44 @@
+package org.emp;
+
+public class Employee {
+	
+	private void empName() {
+      System.out.println("Emp name:anu");
+	}
+	
+	private void empAge() {
+       System.out.println("Emp age:23");
+	}
+	
+	private void empId() {
+         System.out.println("Emp id:2345678");
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.empName();
+		e.empAge();
+		e.empId();
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
